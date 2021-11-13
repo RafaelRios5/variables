f19125854681a5ee6c4308bc7f6c9235d7eea013
RafaelRios5 <rafael.rios@hgs.hiddengeniusproject.org>
2021-11-13 21:45:29 +0000

    Creating a new repository called Variables

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("name");
+  
+  
+  //type idenifier = new type();
+  Scanner scanner = new Scanner(System.in);
+  String name = scanner.nextLine();
+  
+  System.out.println("hello my name is Rafael.Rios " + name);
+  }
+}
